830612a7304310cb6214190c25aa9bdd8ba0c573
benlowry <benlowry@outlook.com>
2018-07-18 20:39:02 +0000

    Silent webhooks

--- src/www/api/webhooks/index-stripe-data.js
+++ src/www/api/webhooks/index-stripe-data.js
@@ -20,7 +20,6 @@ module.exports = {
     if (process.env.NODE_ENV !== 'production') {
       lastTestNumber = await global.redisClient.getAsync('testNumber')
     }
-    console.log('[webhook]', stripeEvent.type, stripeEvent.data.object.id)
     let invoice, charge, customerid, subscriptionid, planid, productid, cardid
     switch (stripeEvent.type) {
       case 'invoice.created':

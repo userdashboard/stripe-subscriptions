76d73008aef0d54f8cfe6a6023deffd7f4b9e1ac
Ben Lowry <benlowry@outlook.com>
2018-08-10 11:20:06 -0600

    Create customer if not exists

--- src/server/bind-customer.js
+++ src/server/bind-customer.js
@@ -8,11 +8,15 @@ async function afterAuthentication (req) {
   if (req.customer || !req.account) {
     return
   }
+  const queryWas = req.query
   const customerid = await dashboard.RedisObject.getProperty(req.account.accountid, 'customerid')
   if (customerid) {
-    const queryWas = req.query
     req.query = {customerid}
     req.customer = await global.api.user.subscriptions.Customer.get(req)
     req.query = queryWas
+    return
   }
+  req.query = {accountid: req.account.accountid}
+  req.customer = await global.api.user.subscriptions.CreateCustomer.post(req)
+  req.query = queryWas
 }
